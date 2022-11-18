c66c490fa97cec31ce30ddaa68138ee81d61af0a
R0gueCS <achrafhardizi1002@gmail.com>
2022-11-18 13:21:49 +0100

    setting up the login ui in both phone and tablet

--- app/src/main/java/src/estm_news/login.kt
+++ app/src/main/java/src/estm_news/login.kt
@@ -1,12 +1,10 @@
 package src.estm_news
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_item_detail.*
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_login.*
-import kotlinx.android.synthetic.main.fragment_item_list.*
 
 class login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,16 +12,18 @@ class login : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         btnConnect.setOnClickListener {
-            if(login.text.length == 10 && isAnAlpha(login.text[0]) && !password.text.toString().equals("")){
-                val view = Intent(this,ItemDetailHostActivity::class.java)
-                startActivity(Intent(view))
-            }else{
-                Toast.makeText(this,"inputs empty", Toast.LENGTH_SHORT).show();
+            if (password.text.toString().isNotEmpty() && login.text.toString().isNotEmpty()) {
+                if (login.text.length == 10 && isAnAlpha(login.text[0])) {
+                    val view = Intent(this, ItemDetailHostActivity::class.java)
+                    startActivity(Intent(view))
+                } else {
+                    Toast.makeText(this, "inputs empty", Toast.LENGTH_SHORT).show();
+                }
             }
         }
     }
 
-    fun isAnAlpha(char:Char):Boolean{
-        return char >= 'a' && char <= 'z' || char >= 'A' && char <= 'Z'
+    private fun isAnAlpha(char: Char): Boolean {
+        return char in 'a'..'z' || char in 'A'..'Z'
     }
 }
