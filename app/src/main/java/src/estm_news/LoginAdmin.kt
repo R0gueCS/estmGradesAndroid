f7ede657e41f26a65e6bf1848cf2e4602fe0c7dc
anasMokhannate <mokhannateanas@gmail.com>
2022-11-21 18:24:28 +0100

    the last commit bi idni llah

--- app/src/main/java/src/estm_news/LoginAdmin.kt
+++ app/src/main/java/src/estm_news/LoginAdmin.kt
@@ -1,11 +1,34 @@
 package src.estm_news
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
+
 
 class LoginAdmin : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_admin)
+
+        btnConnect.setOnClickListener {
+            if (password.text.toString().isNotEmpty() && login.text.toString().isNotEmpty()) {
+
+                if (login.text.toString().equals("admin") && password.text.toString().equals("admin")) {
+
+                    val view = Intent(this, activity_admin::class.java)
+                    startActivity(Intent(view))
+                    finish()
+                }else{
+                    Toast.makeText(this, "Vous n'êtes pas administrateur !!", Toast.LENGTH_SHORT).show();
+                }
+            }else {
+                Toast.makeText(this, "Veuilliez remplir les champs !!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- app/src/main/java/src/estm_news/fragement_consulter.kt
+++ app/src/main/java/src/estm_news/fragement_consulter.kt
@@ -1,14 +1,21 @@
 package src.estm_news
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import kotlinx.android.synthetic.main.activity_admin.*
+import kotlinx.android.synthetic.main.fragment_fragement_modifier.*
 
 class fragement_consulter : Fragment() {
+    var PREFS_KEY = "prefs"
+    var CNE_KEY = "cne"
 
+    lateinit var sharedPreferences : SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,5 +31,21 @@ class fragement_consulter : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_fragement_consulter, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        consulter.setOnClickListener {
+            sharedPreferences = this.requireActivity().getSharedPreferences(PREFS_KEY, Context.MODE_PRIVATE)
+
+            val editor: SharedPreferences.Editor = sharedPreferences.edit()
+
+            editor.putString(CNE_KEY, cne.text.toString())
+
+            editor.apply()
+            val view = Intent(this.activity, ItemDetailHostActivity::class.java)
+            startActivity(Intent(view))
+        }
+    }
+
 
 }
