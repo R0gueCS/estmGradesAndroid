6ff3031d53a374b30f56540c50c8b8a37468fe49
R0gueCS <achrafhardizi1002@gmail.com>
2022-11-22 09:25:27 +0100

    styled fragments

--- app/src/main/java/src/estm_news/fragement_modifier.kt
+++ app/src/main/java/src/estm_news/fragement_modifier.kt
@@ -1,28 +1,18 @@
 package src.estm_news
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_admin.*
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_fragement_modifier.*
-import kotlinx.android.synthetic.main.fragment_fragement_modifier.noteCours
-import kotlinx.android.synthetic.main.fragment_fragement_modifier.noteTp
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 class fragement_modifier : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -57,31 +47,25 @@ class fragement_modifier : Fragment() {
                         }
                     }
 
-                }else{
-                    Toast.makeText(requireContext(), "il n'éxiste aucun étudiant avec ce cne", Toast.LENGTH_SHORT)
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        "il n'éxiste aucun étudiant avec ce cne",
+                        Toast.LENGTH_SHORT
+                    )
                         .show();
+                }
+                Toast.makeText(requireContext(), "Note modifiée avec succèes", Toast.LENGTH_SHORT)
+                    .show();
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Veuilliez remplir les champs demandés!!",
+                    Toast.LENGTH_SHORT
+                ).show();
             }
-            Toast.makeText(requireContext(), "Note modifiée avec succèes", Toast.LENGTH_SHORT)
-                .show();
-        }else{
-            Toast.makeText(
-                requireContext(),
-                "Veuilliez remplir les champs demandés!!",
-                Toast.LENGTH_SHORT
-            ).show();
-        }
 
+        }
     }
-}
 
-companion object {
-    @JvmStatic
-    fun newInstance(param1: String, param2: String) =
-        fragement_modifier().apply {
-            arguments = Bundle().apply {
-                putString(ARG_PARAM1, param1)
-                putString(ARG_PARAM2, param2)
-            }
-        }
-}
 }
